90602d7abdd1fd0f8d71dba48e9c9c65a700d063
phil0s0pher <c.lambert1213@gmail.com>
2015-11-10 09:48:44 -0800

    v2

--- GrowingTree.rb
+++ GrowingTree.rb
@@ -1,28 +1,22 @@
-
-N, S, E, W = 1, 2, 3, 4
-NE, SW= 5, 6
-NW, SE= 9, 10
-DX         = { E => 1, W => -1, N =>  0, S => 0, NE => 1, SW => -1, NW => -1, SE => 1}
-DY         = { E => 0, W =>  0, N => -1, S => 1, NE => -1, SW => 1, NW => -1, SE => 1}
-OPPOSITE   = { E => W, W =>  E, N =>  S, S => N }
-OPP1	   = {N => NW, S => SW, E => SE, W => SW}
-OPP2	   = {N => NE, S => SE, E => NE, W => NW}
+DX         = { :E => 1, :W => -1, :N =>  0, :S => 0, :NE => 1, :SW => -1, :NW => -1, :SE => 1}
+DY         = { :E => 0, :W =>  0, :N => -1, :S => 1, :NE => -1, :SW => 1, :NW => -1, :SE => 1}
+OPPOSITE   = { :E => :W, :W =>  :E, :N =>  :S, :S => :N }
+OPP1	   = {:N => :NW, :S => :SW, :E => :SE, :W => :SW}
+OPP2	   = {:N => :NE, :S => :SE, :E => :NE, :W => :NW}
 
 class GrowingTree
 	attr_accessor :grid
 
-	def initialize(width,height)
+	def initialize(width, height)
 		#create grid of unvisited cells
-		@width, @height = width, height
-		@grid = Array.new(@height) {Array.new(@width) {"?"}}
+		@grid = Array.new(height) {Array.new(width) {"?"}}
 
-		print "\e[2J"
-		sleep 0.02
-		run(@width, @height)
-		setWall
+		run(width, height)
+		setWall(width, height)
 	end
 
 	def run(width, height)
+		#add first cell to active set
 		x, y = rand(1...width-1), rand(1...height-1)
 		cells = []
 
@@ -30,33 +24,33 @@ class GrowingTree
 
 		until cells.empty?
 			#select
-			index = cells.length-1
-
+			index = rand(cells.length)
  			x, y = cells[index]
- 			[N,S,E,W].shuffle(random: Random.new(Math::PI)).each do |dir|
+
+ 			#check neighbors
+ 			[:N,:S,:E,:W].shuffle.each do |dir|
  				dx, dy = x+DX[dir], y+DY[dir]
 
- 				if dx > 0 && dx < @width-1 && dy > 0 && dy < @height-1 && @grid[dy][dx] == "?" && check(dir, dx, dy)
+ 				if dx > 0 && dx < width-1 && dy > 0 && dy < height-1 && @grid[dy][dx] == "?" && check(dir, dx, dy)
+ 					#if neighbor is not visited add to active set
  					@grid[y][x] = " "
  					@grid[dy][dx] = " "
  					cells << [dx,dy]
  					index = nil
-
- 					myPrint
- 					sleep 0.02
-
- 					break
+					break
  				end
  			end
+ 			#If cell has no neighbors remove the cell from the active set
  			cells.delete_at(index) if index
  		end
- 		myPrint
 	end
 
 	def check(dir, x, y)
 
 		status = []
-		[N,S,E,W,NE,NW,SW,SE].each {|d| 
+
+		#check that the cell is surrounded by unvisited cells
+		[:N,:S,:E,:W,:NE,:NW,:SW,:SE].each {|d| 
 			if d != OPPOSITE[dir] &&  d != OPP1[OPPOSITE[dir]] && d != OPP2[OPPOSITE[dir]]
 				dx, dy = x+DX[d],y+DY[d]
 				if @grid[dy][dx] == "?"
@@ -73,13 +67,10 @@ class GrowingTree
 		end
 	end
 
-	def myPrint
-		puts @grid.map {|row| row.join("")}
-	end
-
-	def setWall
-	 	for y in 0...@height
-	 		for x in 0...@width
+	def setWall(width,height)
+		#replace unvisited cells with walls
+	 	for y in 0...height
+	 		for x in 0...width
 	 			if @grid[y][x] == "?"
 	 				@grid[y][x] = "w"
 	 			end
